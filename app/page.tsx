dc7ec3b684d38b98eef6a2ed22f3958170f416d3
rafal <pacykrafal@gmail.com>
2026-05-29 17:12:07 +0200

    Fix search page build2

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Price Scanner</h1>
-      <p>System monitorowania cen produktów.</p>
-    </main>
-  );
+  redirect("/search");
 }
